2927c465914921250ce329b61dbfa3b2ac57cbd3
SatyaValli-Andey <satyavalliandey@gmail.com>
2024-08-01 11:24:08 +0000

    Update App.java

--- src/main/java/com/accenture/maven/App.java
+++ src/main/java/com/accenture/maven/App.java
@@ -1,20 +1,21 @@
-package com.acc.maven;
-
+package com.acc.lkm.maven.Mavenprojectnewsam;
 import java.util.SortedMap;
 import java.util.TreeMap;
-
-public class MarketProjects {
+import java.util.*;
+public class SortedMapProg {
     public static void main(String[] args) {
         SortedMap<String, String> marketProjects = new TreeMap<>();
         
-       
+        // Adding markets and their projects
         marketProjects.put("North America", "Project A");
         marketProjects.put("Europe", "Project B");
         marketProjects.put("Asia", "Project C");
         
-        
-        for (Map.Entry<String, String> entry : marketProjects.entrySet()) {
+        // Displaying the sorted map
+        for (Map.Entry entry : marketProjects.entrySet()) {
             System.out.println("Market: " + entry.getKey() + ", Project: " + entry.getValue());
         }
     }
 }
+
+
